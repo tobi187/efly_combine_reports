1b3197a2d209fca0d482d2227c4544411379e6a0
tobi187 <fischerstobi1998@gmail.com>
2022-06-05 14:11:54 +0200

    ss

--- excelTemplateWorker.py
+++ excelTemplateWorker.py
@@ -4,19 +4,33 @@ import pandas as pd
 
 DATA_SHEET_NAME = "data"
 
-COLUMN_NAMES = ["Beworbene SKU", "Beworbene ASIN", "Impressionen", "Klicks", "Klickrate (CTR)", "Kosten pro Klick (CPC)", "Ausgaben", "7 Tage, Umsatz gesamt (€)", "Gesamtumsatzkosten für Werbung (ACOS)", "Gesamtrendite von Werbeausgaben (Return on Advertising Spend, ROAS)", "7 Tage, Aufträge gesamt (#)", "7Tage, Einheiten gesamt (#)", "7-Tage-Konversionsrate", "7Tage, Beworbene SKU-Einheiten (#)", "7-Tage, Andere SKU-Einheiten (#)", "7Tage, Beworbene SKU-Umsätze (€)", "7-Tage, Andere SKU-Umsätze (€)", "Gruppierung"]
+COLUMN_NAMES = ["Beworbene SKU", "Beworbene ASIN", "Impressionen", "Klicks", "Klickrate (CTR)",
+                "Kosten pro Klick (CPC)", "Ausgaben", "7 Tage, Umsatz gesamt (€)",
+                "Gesamtumsatzkosten für Werbung (ACOS)",
+                "Gesamtrendite von Werbeausgaben (Return on Advertising Spend, ROAS)", "7 Tage, Aufträge gesamt (#)",
+                "7Tage, Einheiten gesamt (#)", "7-Tage-Konversionsrate", "7Tage, Beworbene SKU-Einheiten (#)",
+                "7-Tage, Andere SKU-Einheiten (#)", "7Tage, Beworbene SKU-Umsätze (€)",
+                "7-Tage, Andere SKU-Umsätze (€)", "Gruppierung"]
 
 HEADERS_TO_CHANGE = {
     "Keyword-oderProdukt-Targeting": "Keyword",
-    "GesamtumsatzfürWerbung(ACoS)": "ACOS ",
-    "Verkäufe": "14 Tage, Umsatz gesamt",
-    "14Tage,Einheitengesamt": "Einheiten insgesamt",
+    "acos": "Gesamtumsatzkosten für Werbung (ACOS)",
+    "Verkäufe": "7 Tage, Aufträge gesamt (#)",
+    "Orders": "7 Tage, Umsatz gesamt (€)",
+    "14Tage,Einheitengesamt": "7 Tage, Einheiten gesamt (#)",
+    "Units": "7 Tage, Einheiten gesamt (#)",
     "Anzeigegruppe": "Anzeigegruppenname",
-    "CampaignName(Informationalonly)": "Kampagnen-Name",
-    "BeworbeneSKU": "SKU",
-    "BeworbeneASIN": "ASIN",
-    "AdGroupName(Informationalonly)": "Anzeigegruppenname",
-    "Portfolioname": "Portfolio Name"
+    "Campaign Name(Informational only)": "Kampagnen-Name",
+    "SKU": "Beworbene SKU",
+    "ASIN": "Beworbene ASIN",
+    "ASIN(Informationalonly)": "Beworbene ASIN",
+    "AdGroupName (Informational only)": "Anzeigegruppenname",
+    "Portfolio Name": "Portfolioname",
+    "Clicks": "Klicks",
+    "Click-throughRate": "Klickrate (CTR)",
+    "Spend": "Ausgaben",
+    "Sales": "7 Tage, Umsatz gesamt (€)",
+    "ROAS": "Gesamtrendite von Werbeausgaben (Return on Advertising Spend, ROAS)"
 }
 
 
@@ -24,49 +38,51 @@ class ExcelWorker:
     def __init__(self, fpath: str):
         self.file_path = fpath
         self.row_nr = 2
-        self.col_names = {h_name: index+1 for index, h_name in enumerate(COLUMN_NAMES)}
-        self.double_headers = HEADERS_TO_CHANGE
-        self.unknown_headers = []
+        self.col_names = {h_name.lower(): index + 1 for index, h_name in enumerate(COLUMN_NAMES)}
+        self.double_headers = {h.strip().lower(): i + 1 for i, h in enumerate(HEADERS_TO_CHANGE)}
+        # self.unknown_headers = {"placeholder": len(self.col_names) + 1}
+        self.unknown_headers = ""
+
+    def change_header(self, df: pd.DataFrame) -> pd.DataFrame:
+        for header in df.keys():
+            prepped_header = header.replace(" ", "").lower()
+            if prepped_header in self.double_headers.keys():
+                df.rename({header: self.double_headers[prepped_header]}, axis=1, inplace=True)
 
     def write_data(self, df: pd.DataFrame) -> None:
-        unknown_headers = []
-
         if df.empty:
             return
 
-        for header in df.keys():
-            h: str = header.replace(" ", "")
-            if h.lower() in self.double_headers.keys():
-                df.rename({header: self.double_headers[h.lower()]})
+        df = self.change_header(df)
 
         wb = self.load_wb()
         sheet = wb[DATA_SHEET_NAME]
-
-        for header in df.keys():
-            if header in self.col_names:
-                for row_index, entry in enumerate(df[header]):
-                    sheet.cell(row=row_index+self.row_nr, column=self.col_names[header]).value = entry
+        for col_name in df.keys():
+            if col_name.strip().lower() in [c.lower() for c in self.col_names.keys()]:
+                for index, entry in enumerate(df[col_name]):
+                    sheet.cell(row=index + self.row_nr, column=self.col_names[col_name.strip()]).value = entry
             else:
-                unknown_headers.append(header)
-
-        if any(unknown_headers):
-            for header in unknown_headers:
-                col_index = sheet.max_row + 2
-                sheet.cell(row=1, column=col_index).value = "NOTFOUND: " + header
-                for index, entry in enumerate(df[header]):
-                    sheet.cell(row=index + self.row_nr, column=col_index).value = entry
-
+                self.unknown_headers += col_name + ", "
         self.row_nr += len(df.keys()[0])
         wb.save(self.file_path)
+
+    # def unknown_head_index(self):
+    #     highest = max(self.unknown_headers.values())
+    #     return highest + 1
 
     def setup(self):
         wb = self.load_wb()
         if DATA_SHEET_NAME not in wb.sheetnames:
             wb.create_sheet(DATA_SHEET_NAME)
         sheet = wb[DATA_SHEET_NAME]
+
+        cols = [sheet.cell(row=1, column=i + 1).value for i in range(sheet.max_row + 1)]
+        cols2 = [c.strip() for c in cols if c is not None and c != ""]
+        self.col_names = {c: i + 1 for i, c in enumerate(cols2)}
+
         sheet.delete_rows(1, sheet.max_row + 1)
-        for data in self.col_names:
-            sheet.cell(row=1, column=self.col_names[data]).value = data
+        for k, v in self.col_names.items():
+            sheet.cell(row=1, column=v).value = k
         wb.save(self.file_path)
 
     def load_wb(self):
@@ -75,7 +91,6 @@ class ExcelWorker:
             return xl.load_workbook(self.file_path, keep_vba=True, read_only=False)
         else:
             return xl.load_workbook(self.file_path)
-
 
 # old one with spaces
 # HEADERS_TO_CHANGE = {
--- templateStartUp.py
+++ templateStartUp.py
@@ -13,6 +13,7 @@ def workflow(save_file, paths):
         for sheet in wb.sheet_names:
             sheet_df = pd.read_excel(file, engine="openpyxl", sheet_name=sheet)
             excel_worker.write_data(sheet_df)
+    return excel_worker.unknown_headers
 
 
 def search_folder(path):
@@ -23,12 +24,14 @@ def search_folder(path):
 def start():
     folder = sg.popup_get_folder("Wähle den Ordner in dem die Excel Dateien zum zusammfügen liegen\nAlle Dateien mit der Endung xlsx werden verarbeitet", title="ReportAutomationTemplate")
     save_file_path = sg.popup_get_file("Wähle die Datei zum Speichern aus\nBitte schliesse alle Excel Files die du jetzt und vorher ausgewählt hast\nWenn du auf OK drückst startet das Programm", title="ReportAutomationTemplate")
-
+    with_template = sg.Window("Option", [[sg.T("Alles zusammenführen oder per Template erstellen")], [sg.B("Freestyle"), sg.B("Template")]]).read(close=True)
     if folder != "" and folder is not None and save_file_path != "" and save_file_path is not None:
         files = search_folder(folder)
         start_time = time()
-        workflow(save_file_path, files)
-        sg.PopupOK(f"Fertig\nDauer: {time() - start_time}")
+        res = workflow(save_file_path, files)
+
+        sg.PopupOK("Folgende Überschriften wurden nicht gefunden " + res[:-2],
+                   title=f"Fertig Dauer: {time() - start_time}")
 
 
 if __name__ == "__main__":
